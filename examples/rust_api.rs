c9f3b160f052539e169523625ce89f5d090fb1e3
Kartik Singhal <kartik.singhal@quantinuum.com>
2026-01-23 11:14:07 -0600

    fix: remove `_core` from Rust API (#14)

--- examples/rust_api.rs
+++ examples/rust_api.rs
@@ -1,4 +1,4 @@
-use qir_qis::{get_entry_attributes_core, qir_ll_to_bc_core, qir_to_qis_core, validate_qir_core};
+use qir_qis::{get_entry_attributes, qir_ll_to_bc, qir_to_qis, validate_qir};
 use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -6,17 +6,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ll_text = fs::read_to_string("tests/data/adaptive.ll")?;
 
     // Convert LLVM IR text to bitcode
-    let bc_bytes = qir_ll_to_bc_core(&ll_text)?;
+    let bc_bytes = qir_ll_to_bc(&ll_text)?;
 
     // Validate QIR
-    validate_qir_core(&bc_bytes, None)?;
+    validate_qir(&bc_bytes, None)?;
 
     // Get entry point metadata
-    let attributes = get_entry_attributes_core(&bc_bytes)?;
+    let attributes = get_entry_attributes(&bc_bytes)?;
     println!("Required qubits: {attributes:#?}");
 
     // Compile to QIS
-    let qis_bytes = qir_to_qis_core(&bc_bytes, 2, "aarch64", None)?;
+    let qis_bytes = qir_to_qis(&bc_bytes, 2, "aarch64", None)?;
 
     // Write output
     fs::write("output.qis.bc", qis_bytes)?;
